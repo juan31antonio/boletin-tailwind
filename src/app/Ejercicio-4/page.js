c1944020c10cad38e9fe825878373596ca9a4d0b
juan31antonio <juanantoniocorder32@gmail.com>
2025-02-06 13:49:04 +0100

    terminar tailwind

--- src/app/Ejercicio-4/page.js
+++ src/app/Ejercicio-4/page.js
@@ -1,11 +1,56 @@
 export default function Flexbox(){
     return(
         <>
-            <section>
-
+            <section className="flex justify-center items-center flex-col m-4">
+                <p className="text-3xl">Esto es el titulo</p>
+                <p className="text-xl">Esto es el subtitulo</p>
             </section>
-            <section>
-                
+            <section className="flex justify-center items-center m-4">
+                <div className="grid-cols-2">
+                    <div>
+                        <div className="flex flex-col rounded-2xl w-80 bg-[#ffffff] shadow-xl">
+                            <figure className="flex justify-center items-center rounded-2xl">
+                                <img src="https://tailwind-generator.b-cdn.net/images/card-generator/tailwind-card-generator-card-preview.png" alt="Card Preview" className="rounded-t-2xl"/>
+                            </figure>
+                            <div className="flex flex-col p-8">
+                                <div className="text-2xl font-bold   text-[#374151] pb-6">Generator</div>
+                                <div className=" text-lg   text-[#374151]">Leverage a graphical editor to create, design and customize beautiful web components.</div>
+                                <div className="flex justify-end pt-6">
+                                    <button className="bg-[#7e22ce] text-[#ffffff] w-full font-bold text-base  p-3 rounded-lg hover:bg-purple-800 active:scale-95 transition-transform transform">Try it out!</button>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="flex flex-col rounded-2xl w-80 bg-[#ffffff] shadow-xl">
+                            <figure className="flex justify-center items-center rounded-2xl">
+                                <img src="https://tailwind-generator.b-cdn.net/images/card-generator/tailwind-card-generator-card-preview.png" alt="Card Preview" className="rounded-t-2xl"/>
+                            </figure>
+                            <div className="flex flex-col p-8">
+                                <div className="text-2xl font-bold   text-[#374151] pb-6">Generator</div>
+                                <div className=" text-lg   text-[#374151]">Leverage a graphical editor to create, design and customize beautiful web components.</div>
+                                <div className="flex justify-end pt-6">
+                                    <button className="bg-[#7e22ce] text-[#ffffff] w-full font-bold text-base  p-3 rounded-lg hover:bg-purple-800 active:scale-95 transition-transform transform">Try it out!</button>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="flex flex-col rounded-2xl w-80 bg-[#ffffff] shadow-xl">
+                            <figure className="flex justify-center items-center rounded-2xl">
+                                <img src="https://tailwind-generator.b-cdn.net/images/card-generator/tailwind-card-generator-card-preview.png" alt="Card Preview" className="rounded-t-2xl"/>
+                            </figure>
+                            <div className="flex flex-col p-8">
+                                <div className="text-2xl font-bold   text-[#374151] pb-6">Generator</div>
+                                <div className=" text-lg   text-[#374151]">Leverage a graphical editor to create, design and customize beautiful web components.</div>
+                                <div className="flex justify-end pt-6">
+                                    <button className="bg-[#7e22ce] text-[#ffffff] w-full font-bold text-base  p-3 rounded-lg hover:bg-purple-800 active:scale-95 transition-transform transform">Try it out!</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <ul>
+
+                        </ul>
+                    </div>
+                </div>
             </section>
         </>
     );
